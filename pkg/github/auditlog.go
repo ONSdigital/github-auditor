39b6e2d21d3e291d8023dfa02ec80c8c21026da5
John Topley <john.topley@ons.gov.uk>
2020-01-06 11:43:57 +0000

    Function doesn't need to be defined against a pointer to the Client because it doesn't mutate it

--- pkg/github/auditlog.go
+++ pkg/github/auditlog.go
@@ -27,7 +27,7 @@ type (
 )
 
 // FetchAllAuditLogEntries returns all audit log entries for the specified organisation.
-func (c *Client) FetchAllAuditLogEntries(organisation string) ([]AuditEntry, error) {
+func (c Client) FetchAllAuditLogEntries(organisation string) ([]AuditEntry, error) {
 	var auditEntries []AuditEntry
 	var endCursor *string // Using a pointer type allows this to be nil (an empty string isn't a valid cursor).
 
